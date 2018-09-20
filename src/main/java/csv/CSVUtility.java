a835809c7d05f599a850281c1713fc3b400c9931
Thong Huynh <thongh@coutureconsulting.com>
2018-09-20 23:43:22 +0700

    update

--- src/main/java/csv/CSVUtility.java
+++ src/main/java/csv/CSVUtility.java
@@ -1,5 +1,6 @@
 package csv;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 import java.io.Reader;
@@ -8,12 +9,13 @@ import java.nio.file.Paths;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 public class CSVUtility {
 
 	public static void main(String[] args) throws IOException {
-		String lines = calculateCommission("");	
+		String lines = calculateCommission("", "");	
 	}
 	
 	public String getMessage(String name) {
@@ -22,39 +24,63 @@ public class CSVUtility {
 	}
 	
 	@SuppressWarnings({ "null", "resource" })
-	public static String calculateCommission(String filePath) {
+	public static String calculateCommission(String filePath, String destinationPath) {
 		String result = "";
 		String SAMPLE_CSV_FILE_PATH = "";
 		if (filePath == "") {
-			SAMPLE_CSV_FILE_PATH = "C:/temp/users.csv";			
+			SAMPLE_CSV_FILE_PATH = "C:/temp/MOBILEAPP.csv";			
 		} else {
 			SAMPLE_CSV_FILE_PATH = filePath;
 		}
 		try {
 			Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_CSV_FILE_PATH)); 
-			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
-			for (CSVRecord csvRecord : csvParser) {
-				// Accessing Values by Column Index
-                String id = csvRecord.get(0);
-                String name = csvRecord.get(1);
-                String amount = csvRecord.get(2);
-                String cardNo = csvRecord.get(3);
-
-                System.out.println("Record No - " + csvRecord.getRecordNumber());
-                System.out.println("---------------");
-                System.out.println("Id : " + id);
-                System.out.println("Name : " + name);
-                System.out.println("Amount : " + amount);
-                System.out.println("CardNo : " + cardNo);
-                System.out.println("---------------\n\n");
-                
-                result = result + id;
-			}
+			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+					.withFirstRecordAsHeader()
+					.withIgnoreHeaderCase()
+					.withTrim());
+			generateCSV(destinationPath, csvParser);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 		return result;
+	}
+	
+	@SuppressWarnings("resource")
+	private static void generateCSV(String destinationPath, CSVParser csvParser) {
+		String SAMPLE_CSV_FILE_PATH = "";
+		if (destinationPath == "") {
+			SAMPLE_CSV_FILE_PATH = "C:/temp/MOBILEAPP_cal.csv";			
+		} else {
+			SAMPLE_CSV_FILE_PATH = destinationPath;
+		}
+		try {
+			BufferedWriter writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH));
+			CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                    .withHeader("USER PHONE NUMBER", "USER ID NUMBER", "USER FIRST NAME", "USER LAST NAME" 
+                    		,"USER JOB", "REGISTRATIONS COUNT", "COMMISSION"));
+			
+			for (CSVRecord csvRecord : csvParser) {
+				// Accessing Values by Column Index
+                String userPhoneNo = csvRecord.get(0);
+                String userIdNo = csvRecord.get(1);
+                String userFirstName = csvRecord.get(2);
+                String userLastName = csvRecord.get(3);
+                String userJob = csvRecord.get(4);
+                String userRegisCount = csvRecord.get(5);
+                
+                double commission = Integer.parseInt(userRegisCount) * 0.25;
+               
+                csvPrinter.printRecord(userPhoneNo, userIdNo, userFirstName, 
+                		userLastName, userJob, userRegisCount, commission);
+                
+			}
+			csvPrinter.flush();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
